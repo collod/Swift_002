a1446b3b603ba5207cbcef3541119ba37b189bfb
Lorenzo Collodi <lore.1997@live.it>
2018-10-19 13:31:36 +0200

    Initial Commit

---
+++ Swift_002/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_002
+//
+//  Created by Lorenzo Collodi on 19/10/2018.
+//  Copyright © 2018 Lorenzo Collodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
